c0c9410e65111cf4d85b8043d9c2b68563ddc217
Dewald Laubscher <laubscher.dewald@gmail.com>
2020-04-07 18:03:24 +0200

    update code to serve assets from node

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
-var express = require('express'), 
-	bodyParser = require('body-parser');
-
-var app = express();
-
-var port = 8080;
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  path = require("path"),
+  regForFile = /.*\..*/,
+  app = express(),
+  port = 8080;
 
 app.use(bodyParser.json());
 
-app.get("/", function (request, response) {
-	response.send('My website api root');
+app.post("/mail/contact", function (request, response) {
+  var ContactMail = require("./mail/contact.js");
+  var data = request.body;
+  ContactMail.send(data.message);
+  response.send("Email sent");
 });
 
-app.post("/mail/contact", function (request, response) {
-	var ContactMail = require('./mail/contact.js');
-	var data = request.body;
-	ContactMail.send(data.message);
-	response.send('Email sent');
+app.get("*", (req, res) => {
+  if (regForFile.test(req.path)) {
+    res.sendFile(path.resolve("public/dist/" + req.path).replace("//", "/"));
+  } else {
+    res.sendFile(path.resolve("public/dist/index.html"));
+  }
 });
 
 app.listen(port, "127.0.0.1", function (err) {
-	if (err) 
-	{
-		throw new Error(err);
-	}
-	else 
-	{
-		console.log('Running server on port ' + port);
-	}
+  if (err) {
+    throw new Error(err);
+  } else {
+    console.log("Running server on port " + port);
+  }
 });
